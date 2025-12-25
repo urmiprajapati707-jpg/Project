bcd4e9bd3569de3ac9d28f4bcf2e0f60bdf1a308
urmiprajapati707-jpg <urmiprajapati707@gmail.com>
2025-12-25 15:23:04 +0530

    [Urmi prajapati] : added welcome program

---
+++ src/question2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class question2 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in );
+        int A= sc.nextInt();
+        int B = sc.nextInt();
+        int Sum= A+B;
+        System.out.println(A+B);
+    }
+}
