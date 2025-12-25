fa20cc29eb3c47e7dfccd6589c6154bad95797f6
urmiprajapati707-jpg <urmiprajapati707@gmail.com>
2025-12-25 15:29:58 +0530

    [Urmi prajapati] : added  program

---
+++ src/question3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class question3 {
+    public static void main(String agrs[]){
+        Scanner sc= new Scanner (System.in);
+        double C = sc.nextInt();
+        double F= (C*9%5)+32;
+        System.out.println(F);
+
+
+
+    }
+}
